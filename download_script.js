200e2744c45e72e80729babe827e453c73f1a6d0
rakibul-dev <mdrakibul.dev@gmail.com>
2023-12-16 15:51:47 +0600

    refactor: added callback func for reading data after download finished

--- download_script.js
+++ download_script.js
@@ -1,10 +1,39 @@
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
+const folderPath = "./";
 
-function downloadFile(url) {
+function deleteExcelFiles(directoryPath) {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error("Error reading directory:", err);
+      return;
+    }
+
+    files.forEach((file) => {
+      const filePath = path.join(directoryPath, file);
+
+      if (path.extname(filePath).toLowerCase() === ".xlsx") {
+        deleteFile(filePath);
+      }
+    });
+  });
+}
+
+function deleteFile(filePath) {
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error("Error deleting file:", err);
+    } else {
+      console.log(`Deleted file: ${path.basename(filePath)}`);
+    }
+  });
+}
+
+async function downloadFile(url, cb) {
   const filename = path.basename(url);
   const extension = path.extname(filename);
+  //   deleteExcelFiles(folderPath);
 
   const correctedFilename = extension ? filename : filename + ".xlsx";
   const fileStream = fs.createWriteStream(correctedFilename);
@@ -24,6 +53,7 @@ function downloadFile(url) {
       fileStream.on("finish", () => {
         fileStream.close();
         console.log("Download finished");
+        cb();
       });
     } else {
       console.error(`Error: Received status code ${res.statusCode}`);
